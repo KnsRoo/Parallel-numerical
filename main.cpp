998757020f616e69f043ce9fd27198543e6a676b
KnsRoo <knsroo@gmail.com>
2019-02-16 14:13:50 +0300

    added "Monte Carlo" method

--- main.cpp
+++ main.cpp
@@ -4,11 +4,39 @@
 #include <omp.h>
 #include <string>
 #include <time.h>
+#include <random>
+#include <vector>
 
 using namespace std;
 
+void randdouble(double min, double max, double *arr, int n){
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<> dis(min, max);
+    #pragma omp parallel for
+    for (int i = 0; i < n; i++){
+        arr[i] = dis(gen);
+    }
+}
+
+double g(double x, double y){
+    return pow(x+y,2)/x;
+}
+
 double f(double x){
   return (pow (x, 2) + sin (0.48 * (x + 2))) / (exp (pow (x, 2)) + 0.38);
+}
+
+double mc_method(double a, double b, int n, int k){
+    double *x = new double[n], *y = new double[n];
+    randdouble(0.4, 1.0, x, n);
+    randdouble(0.0, 1.0, y, n);
+    int in = 0;
+    #pragma omp parallel for reduction(+:in)
+    for (int i = 0; i < n; i++){
+        if (y[i] < f(x[i])) in++;
+    }
+    return 0.6 * in/n;
 }
 
 double nc_method(double a, double b, int n, int k){
@@ -40,16 +68,14 @@ double trap_method(double a, double b, int n, int k){
 double rect_method(double a, double b, int n, int k){
     double S = 0.0, h = (b-a)/n;
     #pragma omp parallel for reduction(+:S)
-    for (int i = k%2; i < n - (k%3); i++){
-        S += f(a + i*h + (k%5)*h/2);
-    }
+    for (int i = k%2; i < n - (k%3); i++) S += f(a + i*h + (k%5)*h/2);
     return S * h;
 }
 
 void calc(double (*ptr)(double, double, int, int), double a, double b, double eps, int k){
   clock_t start = clock();
-  double res, res1, delta = 1.0;
-  int n = 1000, coef = (k == 0) ? 15. : 3.;
+  double res, delta = 1.0;
+  int n = 100000, coef = (k == 0) ? 15. : 3.;
   while (eps < delta){
     res = (*ptr)(a,b,n,k);
     delta = (*ptr)(a,b,2*n,k) - res;
@@ -60,7 +86,7 @@ void calc(double (*ptr)(double, double, int, int), double a, double b, double ep
 
 int main(int argc, char* argv[]){
   omp_set_num_threads(4);
-  double eps = 0.00001;
+  double eps = 0.0001;
   double a = 0.4, b = 1.0;
   cout << "Result    " << "Delta       "  << "Time" << endl;
   calc(&rect_method, a, b, eps, 10);
@@ -69,5 +95,6 @@ int main(int argc, char* argv[]){
   calc(&trap_method, a, b, eps, -1);
   calc(&simpson_method, a, b, eps, 0);
   calc(&nc_method, a, b, eps, -2);
+  calc(&mc_method, a, b, eps, -2);
   return 0;
 }

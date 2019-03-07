1ff2c9846a64c271790eb01ca86d60c7056b3ebf
KnsRoo <knsroo@gmail.com>
2019-03-07 12:20:38 +0300

    format cout

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <chrono>
 #include <random>
+#include <iomanip> 
 
 typedef double(*function)(double, double);
 
@@ -136,59 +137,60 @@ double trap_method_xy(function g, double a, double b, double c, double d, int n)
 }
 
 void calc(int method){
-    double eps = 0.0001, res, delta = 1.0, absdelta;
+    string names[7] = { "Left rect ", "Right rect ", "Middle rect ", "Trap ", "Simpson ", "3/8 ", "Monte Carlo " };
+    double eps = 0.01, res, delta = 1.0, absdelta;
     double max_abs_dfx = 0.4621316416755651;
     double max_abs_ddfx = 1.3976907037089663;
     double max_abs_ddddfx = 13.522628329646611;
-    int n = 10000, coef = 3;
+    int n = 1000, coef = 3, mk = 1000000;
     double f_a = 0.4, f_b = 1.0, f_c = 0.0, f_d = 1.0;
     double g_a = 1.0, g_b = 3.0, g_c = 0.0, g_d = 3.0;
     auto startTime = chrono::steady_clock::now();
     while (eps < delta){
         switch (method){
-            case 1:{ res = rect_method_x(&f, 0, f_a, f_b, n, 10); delta = rect_method_x(&f, 0, f_a, f_b, 2*n, 10) - res; } break;
-            case 2:{ res = rect_method_x(&f, 0, f_a, f_b, n, 15); delta = rect_method_x(&f, 0, f_a, f_b, 2*n, 15) - res; } break;
-            case 3:{ res = rect_method_x(&f, 0, f_a, f_b, n, 16); delta = rect_method_x(&f, 0, f_a, f_b, 2*n, 16) - res; } break;
-            case 4:{ res = trap_method_x(&f, 0, f_a, f_b, n); delta = trap_method_x(&f, 0, f_a, f_b, 2*n) - res; } break;
-            case 5:{ res = simpson_method_x(&f, 0, f_a, f_b, n); delta = simpson_method_x(&f, 0, f_a, f_b, 2*n) - res; coef = 15; } break;
-            case 6:{ res = nc_method_x(&f, 0, f_a, f_b, 3*n); delta = nc_method_x(&f, 0, f_a, f_b, 6*n) - res; } break;
-            case 7:{ res = mc_method_x(&f, 0, f_a, f_b, f_c, f_d, n); delta = mc_method_x(&f, 0, f_a, f_b, f_c, f_d, 2*n) - res; } break;
-            case 8:{ res = rect_method_xy(&g, g_a, g_b, g_c, g_d, n, 10); delta = rect_method_xy(&g, g_a, g_b, g_c, g_d, 2*n, 10) - res; } break;
-            case 9:{ res = rect_method_xy(&g, g_a, g_b, g_c, g_d, n, 15); delta = rect_method_xy(&g, g_a, g_b, g_c, g_d, 2*n, 15) - res; } break;
-            case 10:{ res = rect_method_xy(&g, g_a, g_b, g_c, g_d, n, 16); delta = rect_method_xy(&g, g_a, g_b, g_c, g_d, 2*n, 16) - res; } break;
-            case 11:{ res = trap_method_xy(&g, g_a, g_b, g_c, g_d, n); delta = trap_method_xy(&g, g_a, g_b, g_c, g_d, 2*n) - res; } break;
-            case 12:{ res = simpson_method_xy(&g, g_a, g_b, g_c, g_d, n); delta = simpson_method_xy(&g, g_a, g_b, g_c, g_d, 2*n) - res; coef = 15; } break;
-            case 13:{ res = nc_method_xy(&g, g_a, g_b, g_c, g_d, 3*n); delta = nc_method_xy(&g, g_a, g_b, g_c, g_d, 6*n) - res; } break;
-            case 14:{ res = mc_method_xy(&g, g_a, g_b, g_c, g_d, n); delta = mc_method_xy(&g, g_a, g_b, g_c, g_d, 2*n) - res; } break;
+            case 0:{ res = rect_method_x(&f, 0, f_a, f_b, n, 10); delta = rect_method_x(&f, 0, f_a, f_b, 2*n, 10) - res; } break;
+            case 1:{ res = rect_method_x(&f, 0, f_a, f_b, n, 15); delta = rect_method_x(&f, 0, f_a, f_b, 2*n, 15) - res; } break;
+            case 2:{ res = rect_method_x(&f, 0, f_a, f_b, n, 16); delta = rect_method_x(&f, 0, f_a, f_b, 2*n, 16) - res; } break;
+            case 3:{ res = trap_method_x(&f, 0, f_a, f_b, n); delta = trap_method_x(&f, 0, f_a, f_b, 2*n) - res; } break;
+            case 4:{ res = simpson_method_x(&f, 0, f_a, f_b, n); delta = simpson_method_x(&f, 0, f_a, f_b, 2*n) - res; coef = 15; } break;
+            case 5:{ res = nc_method_x(&f, 0, f_a, f_b, 3*n); delta = nc_method_x(&f, 0, f_a, f_b, 6*n) - res; } break;
+            case 6:{ res = mc_method_x(&f, 0, f_a, f_b, f_c, f_d, mk); delta = mc_method_x(&f, 0, f_a, f_b, f_c, f_d, 2*mk) - res; } break;
+            case 7:{ res = rect_method_xy(&g, g_a, g_b, g_c, g_d, n, 10); delta = rect_method_xy(&g, g_a, g_b, g_c, g_d, 2*n, 10) - res; } break;
+            case 8:{ res = rect_method_xy(&g, g_a, g_b, g_c, g_d, n, 15); delta = rect_method_xy(&g, g_a, g_b, g_c, g_d, 2*n, 15) - res; } break;
+            case 9:{ res = rect_method_xy(&g, g_a, g_b, g_c, g_d, n, 16); delta = rect_method_xy(&g, g_a, g_b, g_c, g_d, 2*n, 16) - res; } break;
+            case 10:{ res = trap_method_xy(&g, g_a, g_b, g_c, g_d, n); delta = trap_method_xy(&g, g_a, g_b, g_c, g_d, 2*n) - res; } break;
+            case 11:{ res = simpson_method_xy(&g, g_a, g_b, g_c, g_d, n); delta = simpson_method_xy(&g, g_a, g_b, g_c, g_d, 2*n) - res; coef = 15; } break;
+            case 12:{ res = nc_method_xy(&g, g_a, g_b, g_c, g_d, 3*n); delta = nc_method_xy(&g, g_a, g_b, g_c, g_d, 6*n) - res; } break;
+            case 13:{ res = mc_method_xy(&g, g_a, g_b, g_c, g_d, mk); delta = mc_method_xy(&g, g_a, g_b, g_c, g_d, 2*mk) - res; } break;
         }
         n *= 2;
     }
     auto endTime = chrono::steady_clock::now();
     auto runTime = chrono::duration_cast<std::chrono::duration<double>>(endTime-startTime);
         switch (method){
+            case 0:{ absdelta = max_abs_dfx*(pow(f_b-f_a,2)/2); } break;
             case 1:{ absdelta = max_abs_dfx*(pow(f_b-f_a,2)/2); } break;
-            case 2:{ absdelta = max_abs_dfx*(pow(f_b-f_a,2)/2); } break;
-            case 3:{ absdelta = max_abs_ddfx*(pow(f_b-f_a,3)/24); } break;
-            case 4:{ absdelta = max_abs_ddfx*(pow(f_b-f_a,3)/12); } break;
+            case 2:{ absdelta = max_abs_ddfx*(pow(f_b-f_a,3)/24); } break;
+            case 3:{ absdelta = max_abs_ddfx*(pow(f_b-f_a,3)/12); } break;
+            case 4:{ absdelta = max_abs_ddddfx*(pow(f_b-f_a,5)/2880); } break;
             case 5:{ absdelta = max_abs_ddddfx*(pow(f_b-f_a,5)/2880); } break;
-            case 6:{ absdelta = max_abs_ddddfx*(pow(f_b-f_a,5)/2880); } break;
-            case 7:{ absdelta = 3*(((f_b-f_a)/(2*sqrt(3)))/sqrt(n) + ((f_d-f_c)/(2*sqrt(3)))/sqrt(n))/2; } break; // Согласно равномерному закону распредления
+            case 6:{ absdelta = 3*(((f_b-f_a)/(2*sqrt(3)))/sqrt(n) + ((f_d-f_c)/(2*sqrt(3)))/sqrt(n))/2; } break; // Согласно равномерному закону распредления
+            case 7:{  } break;
             case 8:{  } break;
-            case 9:{  } break;
-            case 10:{ } break;
-            case 11:{  } break;
-            case 12:{ } break;
+            case 9:{ } break;
+            case 10:{  } break;
+            case 11:{ } break;
+            case 12:{  } break;
             case 13:{  } break;
-            case 14:{  } break;
         }
-    cout<< res << " " << fabs(delta/coef) << " " << absdelta << " " << runTime.count() << endl;
+    cout<< setw(12) <<names[method%7] << setw(8) << res << " " << setw(11) << fabs(delta/coef) << " " << setw(11) << absdelta << " " << setw(11) << runTime.count() << endl;
 }
 
 int main(int argc, char* argv[]){
-    cout << "Result    " << "Delta       " << "Absdelta "  << "Time" << endl;
+    cout << setw(12) << "Method " << setw(8) << "Result" << " " << setw(11) << "Delta" << " " << setw(11) << "Absdelta"  << " " << setw(11) << "Time" << endl;
     for (int i = 1; i <= 4; i++){
         omp_set_num_threads(i);
-        for (int j = 1; j <= 14; j++){
+        for (int j = 0; j <= 13; j++){
             calc(j);
         }
         cout<<"-----------"<<endl;

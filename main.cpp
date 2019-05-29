a41b9d1eeefd9240676d1603a1e9de355cef8895
KnsRoo <knsroo@gmail.com>
2019-05-29 17:10:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,199 +1,322 @@
-#include <stdio.h>
 #include <iostream>
-#include <math.h>
 #include <omp.h>
-#include <string>
-#include <chrono>
-#include <random>
-#include <iomanip> 
-
-typedef double(*function)(double, double);
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <time.h>
+#include <math.h>
+#include <algorithm>
+#include <functional>
+#include <array>
+#include <iterator>
+#include <utility>
 
 using namespace std;
 
-void randdouble(double min, double max, double *arr, int n){
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_real_distribution<> dis(min, max);
+int irand(int a, int b) {
+	return rand() % (b - a + 1) + a;
+}
+
+bool exist(vector<int> a, int var){
+    bool ret = false;
     #pragma omp parallel for
-    for (int i = 0; i < n; i++){
-        arr[i] = dis(gen);
+    for (int i = 0; i < a.size(); i++){
+        if (a[i] == var) ret = true;
+    }
+    if (ret) return true;
+    return false;
+}
+
+bool exist(vector<pair<int,int>> a, pair<int,int> var){
+    bool ret = false;
+    #pragma omp parallel for
+    for (int i = 0; i < a.size(); i++){
+        if (a[i] == var) ret = true;
+    }
+    if (ret) return true;
+    int temp = var.first;
+    var.first = var.second;
+    var.second = temp;
+    #pragma omp parallel for
+    for (int i = 0; i < a.size(); i++){
+        if (a[i] == var) ret = true;
+    }
+    if (ret) return true;
+    return false;
+}
+
+class Identity{
+    int x, y; float ab = 0;
+    vector<int> matrix;
+
+public:
+
+Identity(int x, int y){
+    this->x = x;
+    this->y = y;
+    #pragma omp parallel
+    {
+    vector<int> privat;
+    #pragma omp for nowait
+    for (int i = 0; i < x*y; i++){
+        privat.push_back(0);
+    }
+    #pragma omp critical
+    this->matrix.insert(this->matrix.end(), privat.begin(), privat.end());
     }
 }
 
-double g(double x, double y){
-    return  ((y < x) and (y > x/2)) ? pow(x+y,2)/x : 0;
+void init(){
+    int j = 0;
+    #pragma omp parallel for
+    for (int i = 0; i < this->y; i++){
+        this->matrix[this->x*i+j] = 1;
+        j++;
+    }
 }
 
-double f(double arg, double x){
-  return (pow (x, 2) + sin (0.48 * (x + 2))) / (exp (pow (x, 2)) + 0.38);
-}
-
-double mc_method_x(function f, double arg, double a, double b, double c, double d, int n){
-    double *x = new double[n], *y = new double[n]; int in = 0;
-    randdouble(a, b, x, n);
-    randdouble(c, d, y, n);
-    #pragma omp parallel for reduction(+:in)
-    for (int i = 0; i < n; i++){ if (y[i] < f(arg, x[i])) in++; }
-    return (b-a)*(d-c) * in/n;
-}
-
-double mc_method_xy(function g, double a, double b, double c, double d, int n){
-    double S = 0.0;
-    double *x = new double[n], *y = new double[n];
-    randdouble(a, b, x, n);
-    randdouble(c, d, y, n);
-    #pragma omp parallel for reduction(+:S)
-    for (int i = 0; i < n; i++) { S+=g(x[i],y[i]);}
-    return ((b-a) * (d-c))/n * S;
-}
-
-double nc_method_x(function f, double arg, double a, double b, int n){
-    double S1 = 0.0, S2 = 0.0, step = (b-a)/n;
-    #pragma omp parallel for reduction(+:S1)
-    for (int i = 1; i < n; i++){ if (i % 3 == 0) S1 += f(arg, a + i*step); }
-    #pragma omp parallel for reduction(+:S2)
-    for (int i = 1; i < n; i++){ if (i % 3 != 0) S2 += f(arg, a + i*step); }
-    return 3./8. * (f(arg, a) + f(arg, b) + 2*S1 + 3*S2) * step;
-}
-
-double nc_method_xy(function g, double a, double b, double c, double d, int n){
-    double x, S1 = 0.0, S2 = 0.0, step = (b-a)/n;
-    #pragma omp parallel for reduction(+:S1)
-    for (int i = 1; i < n; i++){
-        if (i % 3 == 0){
-            x = a + i*step;
-            S1 += nc_method_x(g, x, c, d, n);
+void shuffle(){
+    vector<int> shuffled;
+    vector<int> poses; int temp;
+    while (poses.size() != this->y){
+        temp = irand(0,this->y-1);
+        if (!exist(poses, temp)) poses.push_back(temp);
+    }
+    //#pragma omp parallel for
+    for (int i = 0; i < poses.size(); i++){
+        for (int j = 0; j < this->x; j++){
+            shuffled.push_back(this->matrix[this->x*poses[i]+j]);
         }
     }
-    #pragma omp parallel for reduction(+:S2)
-    for (int i = 1; i < n; i++){
-        if (i % 3 != 0){
-            x = a + i*step;
-            S2 += nc_method_x(g, x, c, d, n);
+    #pragma omp parallel for
+    for (int i = 0; i < this->x*this->y; i++){
+        this->matrix[i] = shuffled[i];
+    }
+}
+
+void ability(vector<int> target){
+    this->ab = 0;
+    for (int i = 0; i < target.size(); i++){
+        this->ab+=this->matrix[i]*target[i];
+    }
+}
+
+bool wrong(){
+    int isum = 0, jsum = 0;
+    bool ret = false;
+    #pragma omp parallel for reduction(+:isum)
+    for (int i = 0; i < this->x; i++){
+        for (int j = 0; j < this->y; j++){
+            isum+=this->matrix[this->x*i+j];
+        }
+        //#pragma omp critical
+        if (isum > 1) ret = true;
+        isum = 0;
+    }
+    if (ret) return true;
+    #pragma omp parallel for reduction(+:jsum)
+    for (int i = 0; i < this->y; i++){
+        for (int j = 0; j < this->x; j++){
+            jsum+=this->matrix[this->y*j+i];
+        }
+        if (jsum > 1) ret = true;
+        jsum = 0;
+    }
+    if (ret) return true;
+    return false;
+}
+
+void mutation(){
+    while (wrong()){
+        int pos, null;
+        #pragma omp parallel for
+        for (int i = 0; i < this->y; i++){
+            int jsum = 0;
+            #pragma omp parallel for reduction(+:jsum)
+            for (int j = 0; j < this->x; j++){
+                jsum+=this->matrix[this->y*j+i];
+            }
+            if (jsum > 1) pos = i;
+            if (jsum == 0) null = i;
+        }
+        int i = whereT(pos);
+        set((i / this->y)*this->y+null, 1); set(i, 0);
+    }
+}
+
+void set(int pos, int value){
+    this->matrix[pos] = value;
+}
+
+float get_ability(){
+    return this->ab;
+}
+
+int getx(){ return this->x; }
+int gety(){ return this->y; }
+
+void print(){
+    for (int i = 0; i < this->x; i++){
+        for (int j = 0; j < this->y; j++){
+            cout<<this->matrix[this->x*i+j]<<" ";  //
+        }
+        cout<<endl;
+    }
+}
+
+int whereT(int i){
+    int pos;
+    for (int j = 0; j < this->y; j++){
+        if (this->matrix[this->y*j+i] == 1) pos = this->y*j+i;
+    }
+    return pos;
+}
+
+int where(int i){
+    int pos;
+    #pragma omp parallel for
+    for (int j = 0; j < this->x; j++){
+        if (this->matrix[this->x*i+j] == 1) pos = this->x*i+j;
+    }
+    return pos;
+}
+
+};
+
+float mean_ability(vector<Identity> pop){
+    float sum = 0;
+    #pragma omp parallel for reduction(+:sum)
+    for (int i = 0; i < pop.size(); i++){
+        sum+=(pop[i].get_ability());
+    }
+    return sum/pop.size();
+}
+
+
+vector<pair<int,int>> combinations_with_replacement(vector<int> nums){
+    vector<pair<int,int>> dekart;
+    for (int i = 0; i < nums.size(); i++){
+        for (int j = 0; j < nums.size(); j++){
+            pair<int,int> temp = make_pair(nums[i], nums[j]);
+            if (!exist(dekart, temp)) dekart.push_back(temp);
         }
     }
-    return 3./8. * (g(a, b) + g(c, d) + 2*S1 + 3*S2) * step;
+    return dekart;
 }
 
-double simpson_method_x(function f, double arg, double a, double b, int n){
-    double S1 = 0.0, S2 = 0.0, step = (b-a)/n;
-    #pragma omp parallel for reduction(+:S1)
-    for (int i = 2; i < n; i+=2) S1 += f(arg, a + i*step);
-    #pragma omp parallel for reduction(+:S2)
-    for (int i = 1; i < n - 1; i+=2) S2 += f(arg, a + i*step);
-    return (f(arg, a) + f(arg, b) + 2*S1 + 4*S2) * step/3.;
-}
-
-double simpson_method_xy(function g, double a, double b, double c, double d, int n){
-    double x, S1 = 0.0, S2 = 0.0, step = (b-a)/n;
-    #pragma omp parallel for reduction(+:S1)
-    for (int i = 2; i < n; i+=2){
-        x = a + i*step;
-        S1 += simpson_method_x(g, x, c, d, n);
+Identity cross(Identity dad, Identity mom){
+    Identity *child = new Identity(dad.getx(), dad.gety());
+    #pragma omp parallel for
+    for (int i = 0; i < dad.gety(); i++){
+        int posd = dad.where(i);
+        int posm = mom.where(i);
+        int posc = (int)(round((posd+posm)/2));
+        child->set(posc, 1);
     }
-    #pragma omp parallel for reduction(+:S2)
-    for (int i = 1; i < n - 1; i+=2){
-        x = a + i*step;
-        S2 += simpson_method_x(g, x, c, d, n);
+    child->mutation();
+    return *child;
+}
+
+vector<Identity> evolution(vector<Identity> pop, float mean, vector<int> target){
+    vector<float> interval, interval2; float sum = 0;
+    vector<int> nums;
+    #pragma omp parallel for reduction(+:sum)
+    for (int i = 0; i < pop.size(); i++){
+        sum+=pop[i].get_ability();
     }
-    return (g(a, b) + g(c, d) + 2*S1 + 4*S2) * step/3.;
-}
-
-double rect_method_x(function f, double arg, double a, double b, int n, int k){
-    double S = 0.0, h = (b-a)/n;
-    #pragma omp parallel for reduction(+:S)
-    for (int i = k%2; i < n - k%3; i++) S += f(arg, a + i*h + (k%5)*h/2);
-    return S * h;
-}
-
-double rect_method_xy(function g, double a, double b, double c, double d, int n, int k){
-    double x, S = 0.0, h = (b-a)/n;
-   #pragma omp parallel for reduction(+:S)
-    for (int i = k%2; i < n - k%3; i++){
-        x = a + i*h + (k%5)*h/2;
-        S += rect_method_x(g, x, c, d, n, k);
+    for (int i = 0; i < pop.size(); i++){
+        float cumsum = (i == 0) ? 0 : interval[i-1];
+        float temp = pop[i].get_ability()/sum*100+cumsum;
+        interval.push_back(temp);
+        interval2.push_back(temp);
     }
-    return S * h;
-}
-
-double trap_method_x(function f, double arg, double a, double b, int n){
-    double S = 0.0, step = (b-a)/n;
-    double diff = (f(arg, a) + f(arg, b)) / 2;
-    #pragma omp parallel for reduction(+:S)
-    for (int i = 1; i < n - 1; i++) S += f(arg, a + i*step);
-    return (diff + S) * step;
-}
-
-double trap_method_xy(function g, double a, double b, double c, double d, int n){
-    double x, S = 0.0, step = (b-a)/n;
-    double diff = (g(a,b) + g(c,d)) / 2 * (b-a);
-    #pragma omp parallel for reduction(+:S)
-    for (int i = 1; i < n - 1; i++){
-        x = a + i*step;
-        S += trap_method_x(g, x, c, d, n);
+    for (int i = 0; i < pop.size(); i++){
+        float temp = (float)irand(0, 99);
+        interval.push_back(temp);
     }
-    return (diff + S) * step;
+    sort(interval.begin(), interval.end());
+    int k = 0;
+    for (int i = 0; i < interval.size(); i++){
+        if (interval[i] != interval2[k]) nums.push_back(k);
+        else k++;
+    }
+    vector<pair<int,int>> dekart;
+    dekart = combinations_with_replacement(nums);
+    int h = dekart.size()-1;
+    vector<int> best;
+    while (best.size() != pop.size()){
+        int temp = irand(0, h);
+        best.push_back(temp);
+    }
+   vector<Identity> newpop;
+   #pragma omp parallel
+   {
+    vector<Identity> newpop_private;
+   #pragma omp for nowait
+   for (int i = 0; i < pop.size(); i++){
+        Identity temp = cross(pop[dekart[best[i]].first], pop[dekart[best[i]].second]);
+        temp.ability(target);
+        newpop_private.push_back(temp);
+    }
+    #pragma omp critical
+    newpop.insert(newpop.end(), newpop_private.begin(), newpop_private.end());
+    }
+   return newpop;
 }
 
-void calc(int method){
-    string names[7] = { "Left rect ", "Right rect ", "Middle rect ", "Trap ", "Simpson ", "3/8 ", "Monte Carlo " };
-    double eps = 0.01, res, delta = 1.0, absdelta;
-    double max_abs_dfx = 0.4621316416755651;
-    double max_abs_ddfx = 1.3976907037089663;
-    double max_abs_ddddfx = 13.522628329646611;
-    int n = 1000, coef = 3, mk = 1000000;
-    double f_a = 0.4, f_b = 1.0, f_c = 0.0, f_d = 1.0;
-    double g_a = 1.0, g_b = 3.0, g_c = 0.0, g_d = 3.0;
-    auto startTime = chrono::steady_clock::now();
-    while (eps < delta){
-        switch (method){
-            case 0:{ res = rect_method_x(&f, 0, f_a, f_b, n, 10); delta = rect_method_x(&f, 0, f_a, f_b, 2*n, 10) - res; } break;
-            case 1:{ res = rect_method_x(&f, 0, f_a, f_b, n, 15); delta = rect_method_x(&f, 0, f_a, f_b, 2*n, 15) - res; } break;
-            case 2:{ res = rect_method_x(&f, 0, f_a, f_b, n, 16); delta = rect_method_x(&f, 0, f_a, f_b, 2*n, 16) - res; } break;
-            case 3:{ res = trap_method_x(&f, 0, f_a, f_b, n); delta = trap_method_x(&f, 0, f_a, f_b, 2*n) - res; } break;
-            case 4:{ res = simpson_method_x(&f, 0, f_a, f_b, n); delta = simpson_method_x(&f, 0, f_a, f_b, 2*n) - res; coef = 15; } break;
-            case 5:{ res = nc_method_x(&f, 0, f_a, f_b, 3*n); delta = nc_method_x(&f, 0, f_a, f_b, 6*n) - res; } break;
-            case 6:{ res = mc_method_x(&f, 0, f_a, f_b, f_c, f_d, mk); delta = mc_method_x(&f, 0, f_a, f_b, f_c, f_d, 2*mk) - res; } break;
-            case 7:{ res = rect_method_xy(&g, g_a, g_b, g_c, g_d, n, 10); delta = rect_method_xy(&g, g_a, g_b, g_c, g_d, 2*n, 10) - res; } break;
-            case 8:{ res = rect_method_xy(&g, g_a, g_b, g_c, g_d, n, 15); delta = rect_method_xy(&g, g_a, g_b, g_c, g_d, 2*n, 15) - res; } break;
-            case 9:{ res = rect_method_xy(&g, g_a, g_b, g_c, g_d, n, 16); delta = rect_method_xy(&g, g_a, g_b, g_c, g_d, 2*n, 16) - res; } break;
-            case 10:{ res = trap_method_xy(&g, g_a, g_b, g_c, g_d, n); delta = trap_method_xy(&g, g_a, g_b, g_c, g_d, 2*n) - res; } break;
-            case 11:{ res = simpson_method_xy(&g, g_a, g_b, g_c, g_d, n); delta = simpson_method_xy(&g, g_a, g_b, g_c, g_d, 2*n) - res; coef = 15; } break;
-            case 12:{ res = nc_method_xy(&g, g_a, g_b, g_c, g_d, 3*n); delta = nc_method_xy(&g, g_a, g_b, g_c, g_d, 6*n) - res; } break;
-            case 13:{ res = mc_method_xy(&g, g_a, g_b, g_c, g_d, mk); delta = mc_method_xy(&g, g_a, g_b, g_c, g_d, 2*mk) - res; } break;
+int loops(vector<int> maxes){
+    int maximum = 0, index;
+    #pragma omp parallel for reduction(max:maximum)
+    for (int i = 0; i < maxes.size(); i++){
+        if (maxes[i] > maximum){
+            maximum = maxes[i];
+            index = i;
         }
-        n *= 2;
     }
-    auto endTime = chrono::steady_clock::now();
-    auto runTime = chrono::duration_cast<std::chrono::duration<double>>(endTime-startTime);
-        switch (method){
-            case 0:{ absdelta = max_abs_dfx*(pow(f_b-f_a,2)/2); } break;
-            case 1:{ absdelta = max_abs_dfx*(pow(f_b-f_a,2)/2); } break;
-            case 2:{ absdelta = max_abs_ddfx*(pow(f_b-f_a,3)/24); } break;
-            case 3:{ absdelta = max_abs_ddfx*(pow(f_b-f_a,3)/12); } break;
-            case 4:{ absdelta = max_abs_ddddfx*(pow(f_b-f_a,5)/2880); } break;
-            case 5:{ absdelta = max_abs_ddddfx*(pow(f_b-f_a,5)/2880); } break;
-            case 6:{ absdelta = 3*(((f_b-f_a)/(2*sqrt(3)))/sqrt(n) + ((f_d-f_c)/(2*sqrt(3)))/sqrt(n))/2; } break; // Согласно равномерному закону распредления
-            case 7:{  } break;
-            case 8:{  } break;
-            case 9:{ } break;
-            case 10:{  } break;
-            case 11:{ } break;
-            case 12:{  } break;
-            case 13:{  } break;
-        }
-    cout<< setw(12) <<names[method%7] << setw(8) << res << " " << setw(11) << fabs(delta/coef) << " " << setw(11) << absdelta << " " << setw(11) << runTime.count() << endl;
+    return index;
 }
 
-int main(int argc, char* argv[]){
-    cout << setw(12) << "Method " << setw(8) << "Result" << " " << setw(11) << "Delta" << " " << setw(11) << "Absdelta"  << " " << setw(11) << "Time" << endl;
-    for (int i = 1; i <= 4; i++){
-        omp_set_num_threads(i);
-        for (int j = 0; j <= 13; j++){
-            calc(j);
+int main()
+{
+    omp_set_num_threads(4);
+    float y, y_prev = 0;
+    const int POP_SIZE = 4;
+    const int LOOP_SIZE = 20;
+    srand(time(NULL));
+    vector<int> target = {100, 150, 90, 200, 200, 100, 70, 150, 250, 80, 70, 100, 190, 100, 120, 200};
+    vector<Identity> Population;
+    vector<int> maxes;
+    vector<Identity> results;
+    for (int i = 0; i < LOOP_SIZE; i++){
+        for (int i = 0; i < POP_SIZE; i++){
+            Identity *temp = new Identity(4,4);
+            temp->init();
+            temp->shuffle();
+            temp->ability(target);
+            Population.push_back(*temp);
+            delete temp;
         }
-        cout<<"-----------"<<endl;
+        y = mean_ability(Population);
+        while (true){
+            Population = evolution(Population, y, target);
+            y = mean_ability(Population);
+            if (fabs(y - y_prev) == 0) break;
+            y_prev = y;
+        }
+        int maximum = 0, index;
+        #pragma omp parallel for reduction(max:maximum)
+        for (int i = 0; i < Population.size(); i++){
+            int temp = Population[i].get_ability();
+            if (temp > maximum){
+                maximum = temp;
+                index = i;
+            }
+        }
+        maxes.push_back(maximum);
+        results.push_back(Population[index]);
     }
+    int max = loops(maxes);
+    cout<<maxes[max]<<endl;
+    results[max].print();
     return 0;
 }

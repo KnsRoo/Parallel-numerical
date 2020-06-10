5f41ddbe453744354f72e342dcc4a3332c62f1c9
KnsRoo <knsroo@gmail.com>
2020-06-10 21:37:43 +0300

    Add files via upload

--- ParaNP/main.cpp
+++ ParaNP/main.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QFontDatabase>
 #include <math.h>
 #include <stdint.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QFontDatabase::addApplicationFont("interface/HelveticaNeueCyr-Roman.ttf");
     MainWindow w;
     w.show();
 
